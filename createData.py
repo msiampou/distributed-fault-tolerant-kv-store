41d3ad8066f59bcea4f5ac7848776e1e1882587a
Maria Despoina Siampou <siampoumaria@gmail.com>
2021-03-19 00:57:10 +0200

    add random multilevel nesting

--- createData.py
+++ createData.py
@@ -1,18 +1,28 @@
 #!/usr/bin/python
 
 import sys, getopt
+import random
 import string
 
-def create_nested(fields, numlines, maxlevel, maxlength, maxkeys, maxk):
+def generate_random_depth(maxlevel, maxkeys):
+   depth = []
    for i in range(maxkeys):
-      if (maxlevel == -1):
+      depth.append(random.randint(0,maxlevel))
+   return depth
+
+def create_nested(fields, numlines, maxlevel, maxlength, maxkeys):
+   if (maxkeys == 0):
+      print("{}", end="")
+      return
+   for i in range(maxkeys):
+      if (maxlevel == 0):
          print("b", end="")
          return
       if (i == 0):
          print("{", end="")
       print("a : ", end="")
-      create_nested(fields, numlines, maxlevel-1, maxlength, maxkeys, maxk)
-      if i != maxkeys-1:
+      create_nested(fields, numlines, maxlevel-1, maxlength, maxkeys)
+      if (i != maxkeys-1):
          print("; ", end="")
    print("} ", end="")
 
@@ -52,5 +62,16 @@ def parse_args(argv):
 if __name__ == "__main__":
    keyfile, numlines, maxlevel, maxlength, maxkeys = parse_args(sys.argv[1:])
    fields = parse_file(keyfile)
-   create_nested(fields, numlines, maxlevel, maxlength, maxkeys, maxlevel)
+   depth = generate_random_depth(maxlevel,maxkeys)
+   for i in range(0,maxkeys):
+      if (i==0):
+         print("{", end="")
+      print("a : ", end="")
+      create_nested(fields, numlines, depth[i], maxlength, random.randint(0,maxkeys))
+      if (i < maxkeys-1):
+         print("; ", end="")
+      else:   
+         print("}", end="")
+   if (maxkeys == 0):
+      print("{}", end="")
    print()
